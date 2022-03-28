bddc0352c843e560a1498b2a14fe726379308ba3
orelz890 <93476230+orelz890@users.noreply.github.com>
2022-03-28 04:25:46 +0300

    Update Notebook_main.cpp

--- sources/Notebook_main.cpp
+++ sources/Notebook_main.cpp
@@ -1,20 +1,22 @@
-// #include "Direction.hpp"
-// #include <iostream>
-// #include <stdexcept>
-// #include <vector>
-// #include <string>
 // #include "Notebook.hpp"
 
 // using ariel::Direction;
 // using ariel::Notebook;
 
 
-    
+// // How to run: g++ -o a.out Notebook.cpp Notebook_main.cpp Notebook.hpp
+
 
 // int main(){
 //     Notebook n;
-    
+//     // n.write(0,0,1,Direction::Horizontal,"hello my friend----------------------------------------------------------------------------------");
+//     // n.write(0,2,3,Direction::Horizontal, "testing");
+//     // // cout << n.get_char_at(0,0,80) << flush;
+//     // n.show(1);
 
+//     std::string st;
+//     st = n.read(0,0,0,Direction::Horizontal,100);
+//     n.show(0);
 
 
 //     return 0;
