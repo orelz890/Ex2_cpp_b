01280522830884d362f878025b2525147fc22f50
orelz890 <93476230+orelz890@users.noreply.github.com>
2022-03-29 20:42:41 +0300

    final

--- sources/Notebook.cpp
+++ sources/Notebook.cpp
@@ -1,152 +1,127 @@
-#include <iterator>
-#include <algorithm>
 #include "Notebook.hpp"
 
-using ariel::Direction;
-using ariel::Notebook;
+const int MIN_CHAR = 32;
+const int MAX_CHAR = 126;
+const int LINE_LEN = 100;
+const int MIN_PAGE = 0;
+const int ZERO = 0;
 
+namespace ariel{
 
-void Notebook::fill_with_under_score(int page, int row, int col, Direction direction, int action_len){
-    if (direction == Direction::Horizontal){
-        // Filling the empty spaces with under score
-        for (int i = Notebook::last_row_filled; i <= row; i++){
-            // notebook.insert({page, unordered_map<int, char[LINE_LEN]>()});
-            for (int j = 0; j <= LINE_LEN; j++){
-                // If its a new char cel fill with under score
-                char current_char = Notebook::get_char_at(page, i, j);
-                // cout << current_char << flush;
-                if (current_char == '\0'){
+    void Notebook::write(int page, int row, int col, Direction direction, string data){
 
-                    Notebook::set_notebook(page, i, j, '_');
+        int data_len = data.length();
+        if(page < MIN_PAGE || row < ZERO || col < ZERO){
+            throw runtime_error("page & row & column position most be positive!");
+        }
+        if(col >= LINE_LEN || (direction == Direction::Horizontal && col + data_len >= LINE_LEN)){
+            throw runtime_error("Writing in illegal columns!");
+        }
+        // Checking for an illigal char
+        for (int i = 0; i < data_len; i++){
+            char curr_char = data[(unsigned int)i];
+
+            if (curr_char < MIN_CHAR || curr_char > MAX_CHAR || curr_char == '~'){
+                throw runtime_error("The char u picked is not printable!");
+            }
+            if (direction == Direction::Horizontal){
+                // Char is legal & not under score, means we cant write! 
+                curr_char = notebook[page][row][col+i];    
+                if (curr_char != '_' && curr_char != '\0'){
+                    throw runtime_error("Cant over write!");
+                }
+            }
+            // Vertical
+            else{
+                curr_char = notebook[page][row+i][col];
+                if(curr_char != '_' && curr_char != '\0'){
+                    throw runtime_error("Cant over write!");
                 }
             }
         }
-    }
-    // Verticacl
-    else{
-        // Filling the empty spaces with under score
-        for (int i = Notebook::last_row_filled; i <= row + action_len; i++){
-            // notebook.insert({page, unordered_map<int, char[LINE_LEN]>()});
-            for (int j = 0; j <= LINE_LEN; j++){
-                // If its a new char cel fill with under score
-                char current_char = Notebook::get_char_at(page, i, j);
-                // cout << current_char << flush;
-                if (current_char == '\0'){
 
-                    Notebook::set_notebook(page, i, j, '_');
+        // Putting the new data
+        if (direction == Direction::Horizontal){
+            for (int i = 0; i < data_len; i++){
+                notebook[page][row][col+i] = data[(unsigned int)i];
+            }
+        }
+        // Verticacl
+        else{
+            for (int i = 0; i < data_len; i++){
+                notebook[page][row+i][col] = data[(unsigned int)i];
+            }
+        }
+
+    }
+
+    string Notebook::read(int page, int row, int col, Direction direction, int readLength){
+
+        if(page < MIN_PAGE || row < MIN_PAGE || col < MIN_PAGE || readLength < MIN_PAGE){
+            throw runtime_error("page & row & column & num of char to read most be positive!");
+        }
+        if(col >= LINE_LEN || (direction == Direction::Horizontal && col + readLength > LINE_LEN)){
+            throw runtime_error("Some of the lines you want to read do not exist!");
+        }
+        string ans;
+        char curr_char = ' ';
+        if (direction == Direction::Horizontal){
+            for (int i = 0; i < readLength; i++){
+                curr_char = notebook[page][row][col+i];
+                if (curr_char == '\0'){
+                    ans += '_';
+                }
+                else{
+                    ans += curr_char;  
+                }
+            }
+        }else{
+            for (int i = 0; i < readLength; i++){
+                curr_char = notebook[page][row+i][col];
+                if (curr_char == '\0'){
+                    ans += '_';
+                }
+                else{
+                    ans += curr_char;
                 }
             }
         }
+        return ans;
     }
-}
 
-void Notebook::write(int page, int row, int col, Direction direction, string data){
+    void Notebook::erase(int page, int row, int col, Direction direction, int eraselength){
 
-    int data_len = data.length();
-    if(page < MIN_PAGE || row < ZERO || col < ZERO){
-        throw runtime_error("page & row & column position most be positive!");
-    }
-    if(col >= LINE_LEN || (direction == Direction::Horizontal && col + data_len >= LINE_LEN)){
-        throw runtime_error("Writing in illegal columns!");
-    }
-    for (int i = 0; i < data_len; i++){
-        // int char_value = data[(unsigned int)i];
-        if (data[(unsigned int)i] < MIN_CHAR || data[(unsigned int)i] > MAX_CHAR || data[(unsigned int)i] == '~'){
-            throw runtime_error("The char u picked is not printable!");
+        if(page < MIN_PAGE || row < MIN_PAGE || col < MIN_PAGE || eraselength < MIN_PAGE){
+            throw runtime_error("page & row & column & num of char to erse most be positive!");
         }
+        if(col >= LINE_LEN || (direction == Direction::Horizontal && col + eraselength > LINE_LEN)){
+            throw runtime_error("Some of the lines you want to erase do not exist!");
+        }
+        // Fill the empty spaces with an under score
         if (direction == Direction::Horizontal){
-            // Char is legal & not under score, means we cant write! 
-            if (Notebook::get_char_at(page, row, col+i) >= MIN_CHAR && Notebook::get_char_at(page, row, col+i) <= MAX_CHAR && Notebook::get_char_at(page, row, col+i) != '_'){
-                throw runtime_error("Cant over write!");
+            for (int i = 0; i < eraselength; i++){
+                notebook[page][row][col+i] = '~';
+            }
+        }else{
+            for (int i = 0; i < eraselength; i++){
+                notebook[page][row+i][col] = '~';
             }
         }
-        // Vertical
-        else if(Notebook::get_char_at(page, row+i, col) >= MIN_CHAR && Notebook::get_char_at(page, row+i, col) <= MAX_CHAR && Notebook::get_char_at(page, row+i, col) != '_'){
-            throw runtime_error("Cant over write!");
+    }
+
+    void Notebook::show(int page){
+
+        if (page < 0){
+            throw runtime_error("The page u chose don't exist!");
         }
-    }
-    // Fill the empty spaces with an under score
-    fill_with_under_score(page, row, col, direction, data_len);
-
-    if (direction == Direction::Horizontal){
-        for (int i = 0; i < data_len; i++){
-            // int char_value = data[(unsigned int)i];
-            Notebook::set_notebook(page, row, col+i, data[(unsigned int)i]);
-        }      
-    }
-    // Verticacl
-    else{
-        for (int i = 0; i < data_len; i++){
-            // int char_value = data[(unsigned int)i];
-            Notebook::set_notebook(page, row+i, col, data[(unsigned int)i]);
+        string ans;
+        ans = "\n==================\nPage %d" + to_string(page) + ":\n==================\n";
+        for (auto row_it = notebook.cbegin()->second.cbegin(); row_it != notebook.cbegin()->second.cend(); ++row_it){
+            for (int i = 0; i < LINE_LEN; i++){
+                ans+= row_it->second[i];
+            }
+            ans+= "\n";
         }
+        cout<< ans;
     }
-
-}
-
-string Notebook::read(int page, int row, int col, Direction direction, int readLength){
-    if(page < MIN_PAGE || row < MIN_PAGE || col < MIN_PAGE || readLength < MIN_PAGE){
-        throw runtime_error("page & row & column & num of char to read most be positive!");
-    }
-    if(col >= LINE_LEN || (direction == Direction::Horizontal && col + readLength > LINE_LEN)){
-        throw runtime_error("Some of the lines you want to read do not exist!");
-    }
-    fill_with_under_score(page,row,col,direction, readLength);
-    string ans;
-    for (int i = 0; i < readLength; i++){
-        if (direction == Direction::Horizontal){
-            // ans += notebook[page][row][col+i];
-            ans += Notebook::get_char_at(page,row,col+i);  
-
-        }
-        else{
-            // ans += notebook[page][row+i][col];
-            ans += Notebook::get_char_at(page, row+i, col);
-        }
-    }
-    return ans;
-}
-
-void Notebook::erase(int page, int row, int col, Direction direction, int eraselength){
-    if(page < MIN_PAGE || row < MIN_PAGE || col < MIN_PAGE || eraselength < MIN_PAGE){
-        throw runtime_error("page & row & column & num of char to erse most be positive!");
-    }
-    if(col >= LINE_LEN || (direction == Direction::Horizontal && col + eraselength > LINE_LEN)){
-        throw runtime_error("Some of the lines you want to erase do not exist!");
-    }
-    // for (int i = 0; i < eraselength; i++){
-    //     // if (direction == Direction::Horizontal && notebook[page][row][col+i] == '~' ||
-    //     if (direction == Direction::Horizontal && Notebook::get_char_at(page, row, col+i) == '~' ||
-    //             direction == Direction::Vertical && Notebook::get_char_at(page, row+i, col) == '~'){
-    //         throw runtime_error("Cant erase the same place twice");
-    //     }
-    // }
-    
-    // Fill the empty spaces with an under score
-    fill_with_under_score(page, row, col, direction, eraselength);
-    
-    for (int i = 0; i < eraselength; i++){
-        if (direction == Direction::Horizontal){
-            // notebook[page][row][col+i] = '~';
-            Notebook::set_notebook(page, row, col+i, '~');
-        }
-        else{
-            Notebook::set_notebook(page, row+i, col, '~');
-        }
-    }
-}
-
-
-void Notebook::show(int page){
-    if (page < 0){
-        throw runtime_error("The page u chose don't exist!");
-    }
-    std::cout << "\n==================\nPage " << page << ":" << "\n==================\n";
-    for (auto row_it = notebook.cbegin()->second.cbegin(); row_it != notebook.cbegin()->second.cend(); ++row_it){
-        for (int i = 0; i < LINE_LEN; i++){
-            std::cout << row_it->second[i];
-        }
-        std:: cout << "\n";
-    }
-
 }
