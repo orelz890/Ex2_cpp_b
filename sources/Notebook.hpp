58b87ca882e00ec496db0dfea239a22afebc2987
orelz890 <93476230+orelz890@users.noreply.github.com>
2022-03-29 20:43:08 +0300

    final

--- sources/Notebook.hpp
+++ sources/Notebook.hpp
@@ -5,39 +5,25 @@
 #include <stdexcept>
 #include "Direction.hpp"
 
-using namespace std;
-using namespace ariel;
+#include <locale>
+#include <cctype>
 
-#define MIN_CHAR 32
-#define MAX_CHAR 126
-#define LINE_LEN 100
-#define MIN_PAGE 0
-#define ZERO 0
+using namespace std;
 
 namespace ariel{
     class Notebook{
-        int last_row_filled = 0;
-        int last_row_col_filled = 0;
     private:
-        map <int, map<int, char[LINE_LEN]>> notebook;
-    public:
-        // Notebook(){}
-        // Notebook();
-        // ~Notebook();
+        map <int, map<int, char[100]>> notebook;
 
+    public:
+        Notebook(/*args*/){}
+        ~Notebook(){}
+
+        int last_row_filled = 0;
         void write(int page, int row, int col, Direction direction, string data);
         string read(int page, int row, int col, Direction direction, int readLength);
         void erase(int page, int row, int col, Direction direction, int eraselength);
         void show(int page);
-        void fill_with_under_score(int page, int row, int col, Direction direction, int action_len);
-        void open_new_notebook(){
-            notebook.clear();
-        }
-        void set_notebook(int page, int row, int col, char c){
-            notebook[page][row][col] = c;
-        }
-        char get_char_at(int page, int row, int col){
-            return notebook[page][row][col];
-        }
     };
+
 };
