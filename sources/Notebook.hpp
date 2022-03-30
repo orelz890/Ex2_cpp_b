204023e71b2082ad3767c02188adbe0710049251
orelz890 <93476230+orelz890@users.noreply.github.com>
2022-03-30 14:43:11 +0300

    final

--- sources/Notebook.hpp
+++ sources/Notebook.hpp
@@ -8,6 +8,12 @@
 #include <locale>
 #include <cctype>
 
+const int MIN_CHAR = 32;
+const int MAX_CHAR = 126;
+const int LINE_LEN = 100;
+const int MIN_PAGE = 0;
+const int ZERO = 0;
+
 using namespace std;
 
 namespace ariel{
@@ -24,6 +30,16 @@ namespace ariel{
         string read(int page, int row, int col, Direction direction, int readLength);
         void erase(int page, int row, int col, Direction direction, int eraselength);
         void show(int page);
+        void set_notebook(int page, int row, int col, char c){
+            notebook[page][row][col] = c;
+            if (row > last_row_filled){
+                last_row_filled = row;
+            }
+            
+        }
+        char get_char_at(int page, int row, int col){
+            return notebook[page][row][col];
+        }
     };
 
 };
