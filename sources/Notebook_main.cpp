101cdd267f23005c8ac615c8cf875b1073fcbee8
orelz890 <93476230+orelz890@users.noreply.github.com>
2022-03-30 14:42:17 +0300

    final

--- sources/Notebook_main.cpp
+++ sources/Notebook_main.cpp
@@ -5,19 +5,65 @@
 
 
 // // How to run: g++ -o a.out Notebook.cpp Notebook_main.cpp Notebook.hpp
-
+// namespace ariel{
+//     void print_ans(Notebook& n, int action){
+//         int page, row, col, d, len;
+//         string w;
+//         Direction direction;
+//         if (action > 0 && action < 4){
+//             cout<< "Enter a page, row, col, direction - {Horizental = 1, else 2}\n";
+//             cin>> page>> row >> col>> d;
+//             fflush(stdin);
+//             if (d == 1){
+//                 direction = Direction::Horizontal;
+//             }
+//             else{
+//                 direction = Direction::Vertical;
+//             }
+//         }
+//         switch (action)
+//         {
+//         case 1:
+//             cout<< "Enter a string to write\n";
+//             cin>> w;
+//             n.write(page,row,col,direction, w);
+//             break;
+//         case 2:
+//             cout<< "Enter a length to read\n";
+//             cin>> len;
+//             cout << n.read(page,row,col,direction, len);
+//             break;
+//         case 3:
+//             cout<< "Enter a length to erase\n";
+//             cin>> len;
+//             n.erase(page,row,col,direction, len);
+//             break;
+//         case 4:
+//             cout<< "Enter a page to show\n";
+//             cin>> page;
+//             n.show(page);
+//             break;
+//         default:
+//             cout<< "The input isn't legal\n";
+//             break;
+//         }
+//     }
+// }
 
 // int main(){
 //     Notebook n;
-//     // n.write(0,0,1,Direction::Horizontal,"hello my friend----------------------------------------------------------------------------------");
-//     // n.write(0,2,3,Direction::Horizontal, "testing");
-//     // // cout << n.get_char_at(0,0,80) << flush;
-//     // n.show(1);
+//     int action = -1;
+//     while (1){
+//         cout<<"\nEnter an action number:/n Exit(notebook will be deleted!) = 0, Write = 1 , Read = 2, Erase = 3, Show = 4\n"<<endl;
+//         cin>>action;
+//         fflush(stdin);
+//         if (action == 0){
+//             cout<< "Goodbye my friend\n";
+//             break;
+//         }
+//         ariel::print_ans(n, action);
 
-//     std::string st;
-//     st = n.read(0,0,0,Direction::Horizontal,100);
-//     n.show(0);
-
+//     }
 
 //     return 0;
 // }
